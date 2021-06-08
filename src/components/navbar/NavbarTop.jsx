06741a84c9db8c3b97bf5587dfe67fef01383795
Marko Cekaj <marko.cekaj@amplitudo.me>
2021-06-08 14:46:16 +0200

    added logo & style

--- src/components/navbar/NavbarTop.jsx
+++ src/components/navbar/NavbarTop.jsx
@@ -5,7 +5,7 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { t } from "react-switch-lang";
 import "./Navbar.scss";
 import LogoutIcon from "./img/log-out.svg";
-
+import AmplLogo from './img/amplitudo.svg'
 import LanguageOptions from './LanguageOptions'
 
 
@@ -30,36 +30,29 @@ const NavbarTop = (props) => {
 
 	return (
 		<Navbar bg="light" expand="lg">
+			<Navbar.Brand ><Link to="/home"><img src={AmplLogo} alt="" /></Link></Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<NavDropdown title={t('navbar.add')} id="basic-nav-dropdown">
+						<NavDropdown.Item>
+							<Nav.Link as={Link} to="/addNewUser" >{t('navbar.addNewUser')} </Nav.Link>
+						</NavDropdown.Item>
+						<NavDropdown.Item>
+							<Nav.Link as={Link} to="/addNewVehicle" >{t('navbar.addNewVehicle')} </Nav.Link>
+						</NavDropdown.Item>
+						<NavDropdown.Item>
+							<Nav.Link as={Link} to="/addNewReservation" >{t('navbar.addNewReservation')} </Nav.Link>
+						</NavDropdown.Item>
 
-				<div className="d-flex justify-content-end flex-wrap w-100 dd-navbar-container">
-					<div className="d-flex dd-navbar-items">
-						<LanguageOptions />
-						<div
-							className="dropdown-menu-right basic-nav-dropdown2 dropdownNav mx-0 ml-lg-3 dropdown nav-item">
-							<NavDropdown title={`${t('navbar.add')}`}>
-								<NavDropdown.Item>
-									<Nav.Link as={Link} to="/addNewUser" >{t('navbar.addNewUser')} </Nav.Link>
-								</NavDropdown.Item>
-								<NavDropdown.Item>
-									<Nav.Link as={Link} to="/addNewVehicle" >{t('navbar.addNewVehicle')} </Nav.Link>
-								</NavDropdown.Item>
-								<NavDropdown.Item>
-									<Nav.Link as={Link} to="/addNewReservation" >{t('navbar.addNewReservation')} </Nav.Link>
-								</NavDropdown.Item>
-							</NavDropdown>
-							<NavDropdown title='hi user'>
-								<NavDropdown.Item onClick={logout}>
-									<span className="profile-link-text" id="logout-link-text">
-										{t('navbar.logout')}<img src={LogoutIcon} alt="" />
-									</span>
-								</NavDropdown.Item>
-							</NavDropdown>
-						</div>
-
-					</div>
-				</div>
+					</NavDropdown>
+					<LanguageOptions />
+					<NavDropdown.Item onClick={logout} >
+						<span className="profile-link-text" id="logout-link-text" >
+							{t('navbar.logout')}<img src={LogoutIcon} alt="" />
+						</span>
+					</NavDropdown.Item>
+				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
 	);
