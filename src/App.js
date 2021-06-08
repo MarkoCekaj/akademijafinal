e9f55fa41499f2d5637f1c41784f3302b66ee38a
Marko Cekaj <marko.cekaj@amplitudo.me>
2021-06-08 14:43:02 +0200

    lazy loading

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import eng from './language/eng.json';
 import mne from './language/mne.json';
 import { setDefaultTranslations, setDefaultLanguage } from 'react-switch-lang';
 import PrivateRoute from './privateRoute/PrivateRoute';
-import Login from './pages/Login/Login';
-import Home from './pages/Home/Home';
-
+import React, { Suspense } from 'react';
+const Login = React.lazy(() => import('./pages/Login/Login'))
+const Home = React.lazy(() => import('./pages/Home/Home'))
+const Clients = React.lazy(() => import('./pages/Clients/Clients'))
+const Reservations = React.lazy(() => import('./pages/Reservations/Reservations'))
+const Vehicles = React.lazy(() => import('./pages/Vehicles/Vehicles'))
 function App() {
   setDefaultTranslations({ eng, mne });
   const lang = localStorage.getItem('lang');
@@ -15,11 +18,14 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <PrivateRoute path="/" exact component={Login}></PrivateRoute>
-          <PrivateRoute path="/home" exact component={Home}></PrivateRoute>
+          <PrivateRoute path="/" exact component={() => <Suspense fallback={<div>Loading...</div>}><Login /></Suspense>}></PrivateRoute>
+          <PrivateRoute path="/home" exact component={() => <Suspense fallback={<div>Loading...</div>}><Home /></Suspense>}></PrivateRoute>
+          <PrivateRoute path="/vehicles" exact component={() => <Suspense fallback={<div>Loading...</div>}><Vehicles /></Suspense>}></PrivateRoute>
+          <PrivateRoute path="/clients" exact component={() => <Suspense fallback={<div>Loading...</div>}><Clients /></Suspense>}></PrivateRoute>
+          <PrivateRoute path="/reservations" exact component={() => <Suspense fallback={<div>Loading...</div>}><Reservations /></Suspense>}></PrivateRoute>
         </Switch>
       </BrowserRouter>
-    </div>
+    </div >
   );
 }
 
